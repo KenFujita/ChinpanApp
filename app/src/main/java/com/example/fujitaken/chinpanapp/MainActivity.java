e848e9e1f354c7d8ef3d7a83caca3446618cff2c
fujitaken <kentyan199@yahoo.co.jp>
2017-05-30 19:38:40 +0900

    add function

--- app/src/main/java/com/example/fujitaken/chinpanapp/MainActivity.java
+++ app/src/main/java/com/example/fujitaken/chinpanapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.fujitaken.chinpanapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import static com.example.fujitaken.chinpanapp.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onAdd(android.view.View v){
+        int a=0;
+
+        a++;
     }
 }
