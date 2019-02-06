a157d9780bdf0e3b0c8189343617ff475942b1b9
fujitaken <kentyan199@yahoo.co.jp>
2019-02-06 23:13:02 +0900

    prepare the function

--- app/src/main/java/com/example/fujitaken/chinpanapp/MainActivity.java
+++ app/src/main/java/com/example/fujitaken/chinpanapp/MainActivity.java
@@ -2,21 +2,24 @@ package com.example.fujitaken.chinpanapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-
-import static com.example.fujitaken.chinpanapp.R.styleable.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tx;
+    int a=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tx = (TextView) findViewById(R.id.textview);
+        //Button button = (Button)findViewById(R.id.button);
     }
 
     public void onAdd(android.view.View v){
-        int a=0;
-
         a++;
+        tx.setText(String.valueOf(a));
     }
 }
