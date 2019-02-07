4866cd83f12f162205e9037099fc6bd6e99997a9
fujitaken <kentyan199@yahoo.co.jp>
2019-02-07 23:32:55 +0900

    画像にアニメーションを追加 ConstraintLayoutを使用する為の細かい変更

--- app/src/main/java/com/example/fujitaken/chinpanapp/MainActivity.java
+++ app/src/main/java/com/example/fujitaken/chinpanapp/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.fujitaken.chinpanapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tx;
+    ImageView im;
+    Animation am;
     int a=0;
 
     @Override
@@ -14,12 +19,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tx = (TextView) findViewById(R.id.textview);
-        //Button button = (Button)findViewById(R.id.button);
+        tx = findViewById(R.id.textView2);
+        im = findViewById(R.id.imageView);
+
+        am = AnimationUtils.loadAnimation(this,R.anim.translate_animation);
     }
 
     public void onAdd(android.view.View v){
         a++;
         tx.setText(String.valueOf(a));
+
+        im.startAnimation(am);
     }
 }
